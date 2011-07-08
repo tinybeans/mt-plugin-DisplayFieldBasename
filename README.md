270b5432b071ffbde184af481bf82df13192d964
tinybeans <register@tinybeans.net>
2011-07-08 11:38:33 +0900

    First commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# About
+
+Add basename to the Customfield list screen.
+
+# Author
+
+Tomohiro Okuwaki
+http://www.tinybeans.net/blog/
---
+++ plugins/DisplayFieldBasename/lib/DisplayFieldBasename/L10N/ja.pm
@@ -0,0 +1,12 @@
+package DisplayFieldBasename::L10N::ja;
+
+use strict;
+use base 'DisplayFieldBasename::L10N';
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+    'Add basename to the Customfield list screen.' => 'カスタムフィールド一覧画面にベースネームを追加します。',
+    'basename' => 'ベースネーム',
+);
+
+1;
